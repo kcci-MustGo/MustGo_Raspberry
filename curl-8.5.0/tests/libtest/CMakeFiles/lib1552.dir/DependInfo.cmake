f91203db60b7c6daffcb878227d9f04588c934f2
rojong-400 rojong <rhodo11193@gmail.com>
2024-01-15 16:06:38 +0900

    trying

---
+++ curl-8.5.0/tests/libtest/CMakeFiles/lib1552.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rojong/MustGo_raspi/curl-8.5.0/lib/timediff.c" "tests/libtest/CMakeFiles/lib1552.dir/__/__/lib/timediff.c.o" "gcc" "tests/libtest/CMakeFiles/lib1552.dir/__/__/lib/timediff.c.o.d"
+  "/home/rojong/MustGo_raspi/curl-8.5.0/tests/libtest/first.c" "tests/libtest/CMakeFiles/lib1552.dir/first.c.o" "gcc" "tests/libtest/CMakeFiles/lib1552.dir/first.c.o.d"
+  "/home/rojong/MustGo_raspi/curl-8.5.0/tests/libtest/lib1552.c" "tests/libtest/CMakeFiles/lib1552.dir/lib1552.c.o" "gcc" "tests/libtest/CMakeFiles/lib1552.dir/lib1552.c.o.d"
+  "/home/rojong/MustGo_raspi/curl-8.5.0/tests/libtest/testutil.c" "tests/libtest/CMakeFiles/lib1552.dir/testutil.c.o" "gcc" "tests/libtest/CMakeFiles/lib1552.dir/testutil.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/rojong/MustGo_raspi/curl-8.5.0/lib/CMakeFiles/libcurl_shared.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
